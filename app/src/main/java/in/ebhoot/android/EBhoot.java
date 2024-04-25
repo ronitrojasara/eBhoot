0d1f0f7b2b2a3cfba770225fcc52db18400040a5
ronitrojasara <ronitrojasara@gmail.com>
2024-04-25 15:45:36 +0530

    App Colors Updated

--- app/src/main/java/in/ebhoot/android/EBhoot.java
+++ app/src/main/java/in/ebhoot/android/EBhoot.java
@@ -1,11 +1,9 @@
 package in.ebhoot.android;
 
-import com.google.android.material.color.DynamicColors;
-
 public class EBhoot extends android.app.Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        DynamicColors.applyToActivitiesIfAvailable(this);
+//        DynamicColors.applyToActivitiesIfAvailable(this);
     }
 }
